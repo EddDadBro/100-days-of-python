5e5c7e8331ff5c5a50bf56e51d93a37f1ed50317
EddDadBro <edward.duncan1980@gmail.com>
2025-09-05 16:00:30 -0700

    Add Day 2 project and notes

---
+++ Day_02/number_manuipulation.py
@@ -0,0 +1,21 @@
+bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print(int(bmi)) # flooring
+
+print(round(bmi)) # round up or down
+
+print(round(bmi, 2)) # round to a specific decimal place
+
+score = 0
+print (score)
+
+score += 1 # manipulates based on previous value
+print(score)
+
+# f-string
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"Your score is = {score}, your height is {height} and are you winging? {is_winning}")
---
+++ Day_02/type_conversion.py
@@ -0,0 +1,7 @@
+name_of_the_user = input("Enter your name\n")
+lenght_of_name = len(name_of_the_user)
+
+print(type(name_of_the_user)) # stf
+print(type(lenght_of_name)) # int
+
+print("Number of letters in your name: " + str(lenght_of_name))
